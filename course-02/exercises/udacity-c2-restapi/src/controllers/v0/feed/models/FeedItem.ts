1e12cd85f145140f7b78c6418048d3251ddb1118
Kei Fukutani <kifinewera@gmail.com>
2021-04-06 18:08:25 -0700

    update

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/models/FeedItem.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/models/FeedItem.ts
@@ -1,13 +1,26 @@
-import {Table, Column, Model, HasMany, PrimaryKey, CreatedAt, UpdatedAt, ForeignKey} from 'sequelize-typescript';
+import {
+  Table,
+  Column,
+  Model,
+  HasMany,
+  PrimaryKey,
+  CreatedAt,
+  UpdatedAt,
+  ForeignKey,
+} from 'sequelize-typescript';
 import { User } from '../../users/models/User';
 
 @Table
 export class FeedItem extends Model<FeedItem> {
   @Column
+  // '!' indicates that it allows for null.
   public caption!: string;
 
   @Column
   public url!: string;
+
+  // @CreatedAt and @UpdatedAt will set the option in the Postgres database to automatically set the date
+  // when any row is created, or updated and is useful when sorting and filtering our data.
 
   @Column
   @CreatedAt
